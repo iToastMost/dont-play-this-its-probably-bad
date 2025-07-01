b343864276dee1523b2fd8dd0991f9b04777400f
iToastMost <chevre94@gmail.com>
2025-06-30 21:55:53 -0400

    added randomly spawning platforms

--- Main.cs
+++ Main.cs
@@ -3,14 +3,22 @@ using System;
 
 public partial class Main : Node
 {
+    [Signal]
+    public delegate void FallEventHandler();
+
+    [Signal]
+    public delegate void stopFallEventHandler();
+
     [Export]
     public PackedScene PlatformScene { get; set; }
+
+    PackedScene platforms;
 
     public override void _Ready()
     {
         NewGame();
-        var MidPoint = GetNode<Marker2D>("MidPoint").Position;
         var playerPosition = GetNode<Player>("Player").Position;
+        platforms = PlatformScene;
     }
     public void NewGame()
     {
@@ -18,4 +26,37 @@ public partial class Main : Node
         var startPosition = GetNode<Marker2D>("StartPosition");
         player.Start(startPosition.Position);
     }
+
+    private void OnArea2dBodyEntered(Node2D body)
+    {
+        EmitSignal(SignalName.Fall);
+        GetTree().CallGroup("platforms", "Fall");
+        GetNode<Timer>("PlatformTimer").Start();
+        GD.Print("Entered");
+    }
+
+    private void OnArea2dBodyExited(Node2D body)
+    {
+        EmitSignal(SignalName.stopFall);
+        GetNode<Timer>("PlatformTimer").Stop();
+        GetTree().CallGroup("platforms", "StopFall");
+        GD.Print("Exited");
+    }
+
+    private void OnPlatformTimerTimeout()
+    {
+        Platform platform = PlatformScene.Instantiate<Platform>();
+
+        var platformSpawnLocation = GetNode<PathFollow2D>("PlatformPath/PlatformSpawnLocation");
+        platformSpawnLocation.ProgressRatio = GD.Randf();
+
+        platform.Position = platformSpawnLocation.Position;
+
+        GD.Print("Platform Spawned");
+
+        AddChild(platform);
+
+        platforms = PlatformScene;
+    }
+
 }
--- Scenes/Platform.cs
+++ Scenes/Platform.cs
@@ -1,20 +1,54 @@
 using Godot;
 using System;
 
-public partial class Platform : Node2D
+public partial class Platform : CharacterBody2D
 {
+    public const float fallVelocity = 350.0f;
+
+    private bool hasEntered;
 
     public override void _Ready()
     {
-        
+        hasEntered = false;
     }
     public override void _PhysicsProcess(double delta)
     {
-        
+        Vector2 velocity = Velocity;
+
+        if (hasEntered == true)
+        {
+            //var coll = GetNode<CollisionShape2D>("CollisionShape2D").Disabled = true;
+            SetCollisionMaskValue(1, false);
+            velocity.Y = fallVelocity;
+            MoveAndSlide();
+        }
+        else
+        {
+            //var coll = GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+            SetCollisionMaskValue(1, true);
+            velocity.Y = 0;
+        }
+
+        velocity.X = 0;
+
+        Velocity = velocity;
+
+        //MoveAndSlide();
+    }
+
+    public void Fall()
+    {
+        hasEntered = true;
+    }
+
+    public void StopFall()
+    {
+        hasEntered = false;
     }
 
     private void OnVisibleOnScreenNotifier2DScreenExited()
     {
         QueueFree();
+        GD.Print("Platform Freed");
     }
 }
