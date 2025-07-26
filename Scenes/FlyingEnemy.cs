89b2dd325040547fa881525ea0642b253f554977
iToastMost <chevre94@gmail.com>
2025-07-25 23:33:03 -0400

    added movement to flying enemy using sin waves. Todo, change all tweens to sin based movevment for moving platforms....

--- Scenes/FlyingEnemy.cs
+++ Scenes/FlyingEnemy.cs
@@ -4,80 +4,37 @@ using System.Collections.Generic;
 
 public partial class FlyingEnemy : Node2D
 {
-    [Export]
-    private Vector2 offSet = new Vector2(405, 0);
+    private float _time = 0f;
 
     [Export]
-    private Vector2 onSet = new Vector2(0, 0);
+    private float SineAmplitude = 200;
 
     [Export]
-    private float duration = 5.0f;
+    private float SineSpeed = 1f;
 
-    private float verticalSpeed = 100f;
+    [Export]
+    private float UpwardSpeed = 100f;
 
-    private Tween _initialTween;
-    private Tween _loopingTween;
+    private Vector2 _StartPosition;
+
+
+    private Tween tween;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         AddToGroup("Enemies");
-        offSet.Y = Position.Y;
-        onSet.Y = Position.Y;
-        //StartTween();
+        _StartPosition = GlobalPosition;
     }
 
     public override void _Process(double delta)
     {
-        GlobalPosition += new Vector2(0, -(float)delta * verticalSpeed);
+        _time += (float)delta;
 
+        float xOffset = Mathf.Sin(_time * SineSpeed) * SineAmplitude;
+        float yOffset = -(float)_time * UpwardSpeed;
 
-    }
+        GlobalPosition = _StartPosition + new Vector2(xOffset, yOffset);
 
-    private void StartTween()
-    {
-        InitialTween();
-    }
-
-    private void InitialTween()
-    {
-        var body = GetNode<AnimatableBody2D>("AnimatableBody2D");
-        Vector2 start = body.GlobalPosition;
-        Vector2 end = offSet;
-
-        float fullDistance = offSet.DistanceTo(onSet);
-        float baseSpeed = fullDistance / duration;
-
-        float initialDistance = start.DistanceTo(end);
-        float initialDuration = initialDistance / baseSpeed;
-
-        _initialTween = GetTree().CreateTween().SetProcessMode(Tween.TweenProcessMode.Physics);
-
-
-        _initialTween.TweenProperty(body, "global_position", offSet, initialDuration).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
-
-        _initialTween.TweenCallback(Callable.From(() => StartLoopTween(body)));
-    }
-
-    private void StartLoopTween(AnimatableBody2D body)
-    {
-        _loopingTween = GetTree().CreateTween().SetProcessMode(Tween.TweenProcessMode.Physics);
-
-        _loopingTween.SetLoops().SetParallel(false);
-        _loopingTween.TweenProperty(body, "global_position", onSet, duration / 2).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
-        _loopingTween.TweenProperty(body, "global_position", offSet, duration / 2).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
-    }
-
-    public void FreeMovingPlatform()
-    {
-        _initialTween?.Kill();
-        _loopingTween?.Kill();
-
-        SetProcess(false);
-    }
-
-    public override void _ExitTree()
-    {
-        FreeMovingPlatform();
     }
 
     public void OnBodyEntered(Node2D body)
